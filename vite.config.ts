d5b9dee2384ca5002e3a36250da203e13e2f93e1
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-03-18 12:42:56 +0700

    init with unocss

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import unocss from "unocss/vite";
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [unocss(), sveltekit()]
+});
